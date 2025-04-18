a507e1353b2df42b6f7cf459fdb324130bc74781
Hana Mzoughi <hanamzoughi20@gmail.com>
2025-04-18 21:26:00 +0200

    autre modification

--- etudiant.py
+++ etudiant.py
@@ -1,5 +1,7 @@
 def ajouter_etudiant(nom):
     print(f"étudiant {nom} ajouté avec succés.")
+    print("ajout de l'etudiant")
+    return nom
 
 def ajouter_note(nom,note):
     print(f"{nom} a obtenu une note de {note}.")
