7a8c3cd2c018fb568c04b16aed75ec3385c4f319
Hana Mzoughi <hanamzoughi20@gmail.com>
2025-04-18 20:50:42 +0200

    Modification de ajouter_etudiant et ajout de ajouter_note

--- etudiant.py
+++ etudiant.py
@@ -1,2 +1,7 @@
 def ajouter_etudiant(nom):
-    print(f"{nom} a été ajouté.")
+    print(f"étudiant {nom} ajouté avec succés.")
+
+def ajouter_note(nom,note):
+    print(f"{nom} a obtenu une note de {note}.")
+
+    
